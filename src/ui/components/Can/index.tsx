d6452aba62b7fa32cafae372d0d131324dfe1b6a
mango <busimingi@gmail.com>
2023-02-02 18:47:20 +0900

    feat. 캔 만듬

--- src/ui/components/Can/index.tsx
+++ src/ui/components/Can/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as Img } from "../../../assets/heart.svg";
 
 export default function index() {
   return (
@@ -36,29 +37,59 @@ const CanWrapper = styled.div`
   margin-top: 100px;
   @keyframes rotate {
     0% {
-      transform: rotateY(0deg);
-      -ms-transform: rotateY(0deg);
-      -webkit-transform: rotateY(0deg);
-      -o-transform: rotateY(0deg);
-      -moz-transform: rotateY(0deg);
+      transform: rotateY(0deg) rotateX(20deg) rotateZ(20deg);
+      -ms-transform: rotateY(0deg) rotateX(20deg) rotateZ(20deg);
+      -webkit-transform: rotateY(0deg) rotateX(20deg) rotateZ(20deg);
+      -o-transform: rotateY(0deg) rotateX(20deg) rotateZ(20deg);
+      -moz-transform: rotateY(0deg) rotateX(20deg) rotateZ(20deg);
     }
     100% {
-      transform: rotateX(360deg);
-      -ms-transform: rotateX(360deg);
-      -webkit-transform: rotateX(360deg);
-      -o-transform: rotateX(360deg);
-      -moz-transform: rotateX(360deg);
+      transform: rotateY(360deg) rotateX(20deg) rotateZ(20deg);
+      -ms-transform: rotateY(360deg) rotateX(20deg) rotateZ(20deg);
+      -webkit-transform: rotateY(360deg) rotateX(20deg) rotateZ(20deg);
+      -o-transform: rotateY(360deg) rotateX(20deg) rotateZ(20deg);
+      -moz-transform: rotateY(360deg) rotateX(20deg) rotateZ(20deg);
     }
   }
   animation: rotate 1.5s linear 0s infinite alternate;
 `;
+const LidTop = styled.div`
+  width: 100px;
+  height: 250px;
+  background: radial-gradient(
+    circle,
+    #30d0bd 0%,
+    #0090c4ef 34%,
+    rgba(255, 255, 255, 0.1) 90%
+  );
+  top: 0;
+  position: absolute;
+  perspective: 400px;
+  opacity: 1;
+`;
+const LidBottom = styled.div`
+  width: 100px;
+  height: 250px;
+  top: 0;
+  background: radial-gradient(
+    circle,
+    #30d0bd 0%,
+    #0090c4ef 34%,
+    rgba(1, 1, 1, 0.1) 90%
+  );
+  position: absolute;
+  perspective: 400px;
+  opacity: 1;
+`;
+
+const bottomZ = -125;
 const LidTopWrapper = styled.div`
   transform-style: preserve-3d;
 
   position: relative;
   opacity: 1;
 
-  transform: translate3d(0px, 400px, -130px) rotateX(90deg);
+  transform: translate3d(0px, 400px, ${bottomZ}px) rotateX(90deg);
   && :nth-child(1) {
     background-color: gray;
   }
@@ -75,27 +106,10 @@ const LidTopWrapper = styled.div`
     transform: rotateZ(-45deg);
   }
 `;
-const LidTop = styled.div`
-  width: 100px;
-  height: 250px;
-  background-color: blue;
-  top: 0;
-  position: absolute;
-  perspective: 400px;
-  opacity: 1;
-`;
-const LidBottom = styled.div`
-  width: 100px;
-  height: 250px;
-  top: 0;
-  background-color: gray;
-  position: absolute;
-  perspective: 400px;
-  opacity: 1;
-`;
+
 const LidBottomWrapper = styled.div`
   transform-style: preserve-3d;
-  transform: translate3d(0px, 0px, -130px) rotateX(90deg);
+  transform: translate3d(0px, 0px, ${bottomZ}px) rotateX(90deg);
   position: relative;
   perspective: 400px;
   && :nth-child(1) {
@@ -121,35 +135,77 @@ const WallWrap = styled.div`
   position: relative;
   transform-style: preserve-3d;
   && :nth-child(1) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(135deg) translate3d(0px, 0px, ${Z});
+    /* background-image: url(Img); */
   }
   && :nth-child(2) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(270deg) translate3d(0px, 0px, ${Z});
+    /* background-image: url(Img); */
   }
   && :nth-child(3) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(405deg) translate3d(0px, 0px, ${Z});
   }
   && :nth-child(4) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(540deg) translate3d(0px, 0px, ${Z});
   }
   && :nth-child(5) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(675deg) translate3d(0px, 0px, ${Z});
   }
   && :nth-child(6) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(810deg) translate3d(0px, 0px, ${Z});
   }
   && :nth-child(7) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(945deg) translate3d(0px, 0px, ${Z});
   }
   && :nth-child(8) {
-    background-color: red;
+    /* background: radial-gradient(
+      circle,
+      #30d0bd 0%,
+      #0090c4ef 34%,
+      #00091f6c 70%
+    ); */
     transform: rotateY(1080deg) translate3d(0px, 0px, ${Z});
   }
 `;
@@ -161,5 +217,6 @@ const Wall = styled.div`
   position: absolute;
   top: 0;
   background-color: red;
+  /* background: radial-gradient(circle, #30d0bd 0%, #0090c4ef 34%, #00091f6c 70%); */
   perspective: 400px;
 `;
